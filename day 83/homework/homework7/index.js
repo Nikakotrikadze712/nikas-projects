de3369f20c76ec58116d3d61a9b3c756dc624f71
Nikakotrikadze712 <nikokotrikadze88@gmail.com>
2025-09-19 20:15:47 +0400

    second-commit

---
+++ day 83/homework/homework7/index.js
@@ -0,0 +1,11 @@
+let number1 = 13
+let number2 = Number(prompt('enter any integer: '))
+if (number2 == number1) {
+    console.log('=')
+}
+else if (number2 > number1) {
+    console.log('>')
+}
+else {
+    console.log('<')
+}
--- day 83/homework7/index.js
+++
@@ -1,4 +0,0 @@
-const num1 = 7;
-const num2 = Number(prompt('enter number: '));
-
-console.log(num2 === num1 ? 'we have same numbers' : 'we dont have same numbers');
