7257091d1c06565be967122d152f1fd951cc229f
Nikakotrikadze712 <nikokotrikadze88@gmail.com>
2025-10-14 20:52:09 +0400

    second commit

--- day 87/homework/homework6/script.js
+++ day 87/homework/homework6/script.js
@@ -1,8 +1,9 @@
 let number = 100;
 for (let i = 1; i <= number; i++) {
-  if (i % 2 == 0) {
-    console.log(i + " is Even");
-  } else {
-    console.log(i + " is Odd");
-  }
+    if (i % 2 == 0) {
+        console.log(i + " is Even");
+  } 
+    else {
+        console.log(i + " is Odd");
+    }
 }
--- day 87/homework/homework7/script.js
+++ day 87/homework/homework7/script.js
@@ -1,21 +1,22 @@
-let num1 = prompt("enter first num:");
-let num2 = prompt("enter second num:");
+let num1 = Number(prompt("enter first num:"))
+let num2 = Number(prompt("enter second num:"))
 
 let num3 = 0
 let num4 = 0
 
 if (+num1 < +num2) {
-  num3 = +num1;
-  num4 = +num2;
-} else {
-  num3 = +num2;
-  num4 = +num1;
+    num3 = +num1;
+    num4 = +num2;
+} 
+else {
+    num3 = +num2;
+    num4 = +num1;
 }
 
 let sum = 0;
 
 for (let i = num3; i <= num4; i++) {
-  sum += i;
+    sum += i;
 }
 
 alert("sum is " + sum);
