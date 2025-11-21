7ebc65a6ca01dd27dcdfcc3f175b80a410db4b90
Nikakotrikadze712 <nikokotrikadze88@gmail.com>
2025-11-21 19:42:24 +0400

    second commit

--- day 92/homework/homework5/script.js
+++ day 92/homework/homework5/script.js
@@ -1,7 +1,7 @@
 //მოცემული გაქვთ სია: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]მომხმარებელს შემოატანინეთ ორი რიცხვი. შემოტანილი რიცხვებიდან, ყველაზე პატარა რიცხვი, რომელიც იქნება,  იმ რიცხვიდან გამოიტანეთ მეორე რიცხვის ჩათვლით ყველა ელემენტი, ინდექსების მიხედვით.
 let list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-let num1 = parseInt(prompt('enter first number from 10 to 150:'))
-let num2 = parseInt(prompt('enter second number from 10 to 150:'))
+let num1 = Number(prompt('enter first number from 10 to 150:'))
+let num2 = Number(prompt('enter second number from 10 to 150:'))
 let index1, index2
 if (num1 <= num2) {
     index1 = num1
