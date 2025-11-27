385bdd2d495a977531c624fed200e120e3477aa6
Nikakotrikadze712 <nikokotrikadze88@gmail.com>
2025-11-27 18:45:37 +0400

    second commit

--- day 92/homework/homework7/script.js
+++ day 92/homework/homework7/script.js
@@ -1,29 +1,27 @@
 //მოცემული გაქვთ სია [[1, 0, 0, 1, 0], [0, 1, 0, 0, 0],  [0, 1, 0, 0, 1]] ერთი სია არის და ამ სიის შიგნით სამი სია,თითოეული ეს სია შეაერთეთ და გადაიყვანეთ ათობით სისტემაში. შემდეგ დაალაგეთ კლებადობით.
-let list = [[1, 0, 0, 1, 0], [0, 1, 0, 0, 0],  [0, 1, 0, 0, 1]]
+let list = [[1, 0, 0, 1, 0],[0, 1, 0, 0, 0],[0, 1, 0, 0, 1]]
 
-i = 0
-j = 0
-k = 0
+let index1 = []
 
-for (let i = 0; i < list.length - 1; i++) {
-    let index = i
-    for (let j = i + 1; j < list.length; j++) {
-        let num1 = 0
-        for (let k = 0; k < list[index].length; k++) {
-            num1 = num1 * 2 + list[index][k]
-        }
-        let num2 = 0
-        for (let k = 0; k < list[j].length; k++) {
-            num2 = num2 * 2 + list[j][k]
-        }
-        if (num2 > num1) {
-            index = j
-        }
+for (let i = 0;i < list.length;i++) {
+    let index3 = ""
+
+    for (let j = 0;j < list[i].length;j++) {
+        index3 += list[i][j]
     }
-    if (index !== i) {
-        let index2 = list[i]
-        list[i] = list[index]
-        list[index] = index2
+
+    let index2 = parseInt(index3, 2)
+    index1.push(index2)
+}
+
+for (let i = 0; i < index1.length; i++) {
+    for (let j = 0; j < index1.length - 1; j++) {
+        if (index1[j] < index1[j + 1]) {
+            let index4 = index1[j]
+            index1[j] = index1[j + 1]
+            index1[j + 1] = index4
+        }
     }
 }
-console.log(list)
+
+console.log(index1)
