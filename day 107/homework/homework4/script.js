7b6b6f11e852ceb3ef8eb320233b5ecbd3d9ca0b
Nikakotrikadze712 <nikokotrikadze88@gmail.com>
2026-03-14 21:44:09 +0400

    second commit

--- day 107/homework/homework4/script.js
+++ day 107/homework/homework4/script.js
@@ -1,21 +1,23 @@
 //შექმენით ფუნქცია რომელიც არგუმენტად გადაცემულ ორობით სისტემაში მყოფ რიცხვს გადაიყვანს ათობით სისტემაში.
-let function1 = (num1) => {
-    let result1 = ""
-    while (num1 > 0) {
-        result1 = (num1 % 2) + result1
-        num1 = Math.floor(num1 / 2)
+let function1 = num1 => {
+    let result1 = 0
+    let array1 = 0
+    for (let i = num1.length - 1; i >= 0; i--) {
+        result1 = result1 + Number (num1 [i]) * (2 ** array1)
+        array1 = array1 + 1
     }
     return result1
 }
 
 let function2 = function (num2) {
-    let result2 = ""
-    while (num2 > 0) {
-        result2 = (num2 % 2) + result2
-        num2 = Math.floor(num2 / 2)
+    let result2 = 0
+    let array2 = 0
+    for (let i = num2.length - 1; i >= 0; i--) {
+        result2 = result2 + Number (num2 [i]) * (2 ** array2)
+        array2 = array2 + 1
     }
     return result2
 }
 
-console.log (function1 (231))
-console.log (function2 (562))
+console.log (function1 ("101101001"))
+console.log (function2 ("010010110"))
